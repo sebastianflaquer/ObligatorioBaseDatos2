b0429121775b9c188c11725b91b17c2f3d97fc35
Sebastian Flaquer <sebastianflaquer@hotmail.com>
2015-10-21 21:52:35 -0300

    ionsert Paises

--- Script-Insert-Datos.sql
+++ Script-Insert-Datos.sql
@@ -1,252 +1,251 @@
+select *
+from pais
+
 
 insert into pais values
 	('AFG', 'Afganistán'),
-	('ALA', 'Islas Gland')
+	('ALA', 'Islas Gland'),
 	('ALB', 'Albania'),
 	('DEU', 'Alemania'),
-	('AND', 'Andorra'); 
-	('AGO', 'Angola'); 
-	('AIA', 'Anguilla'); 
-	('ATA', 'Antártida'); 
-	('ATG', 'Antigua y Barbuda'); 
-	('ANT', 'Antillas Holandesas'); 
-	('SAU', 'Arabia Saudí'); 
-	('DZA', 'Argelia'); 
-	('ARG', 'Argentina'); 
-	('ARM', 'Armenia'); 
-	('ABW', 'Aruba'); 
-	('AUS', 'Australia'); 
-	('AUT', 'Austria'); 
-	('AZE', 'Azerbaiyán'); 
-	('BHS', 'Bahamas'); 
-	('BHR', 'Bahréin'); 
-	('BGD', 'Bangladesh'); 
-	('BRB', 'Barbados'); 
-	('BLR', 'Bielorrusia'); 
-	('BEL', 'Bélgica'); 
-	('BLZ', 'Belice'); 
-	('BEN', 'Benin'); 
-	('BMU', 'Bermudas'); 
-	('BTN', 'Bhután'); 
-	('BOL', 'Bolivia'); 
-	('BIH', 'Bosnia y Herzegovina'); 
-	('BWA', 'Botsuana'); 
-	('BVT', 'Isla Bouvet'); 
-	('BRA', 'Brasil'); 
-	('BRN', 'Brunéi'); 
-	('BGR', 'Bulgaria'); 
-	('BFA', 'Burkina Faso'); 
-	('BDI', 'Burundi'); 
-	('CPV', 'Cabo Verde'); 
-	('CYM', 'Islas Caimán'); 
-	('KHM', 'Camboya'); 
-	('CMR', 'Camerún'); 
-	('CAN', 'Canadá'); 
-	('CAF', 'República Centroafricana'); 
-	('TCD', 'Chad'); 
-	('CZE', 'República Checa'); 
-	('CHL', 'Chile'); 
-	('CHN', 'China'); 
- (48, 196, 'CY', 'CYP', 'Chipre'); 
- (49, 162, 'CX', 'CXR', 'Isla de Navidad'); 
- (50, 336, 'VA', 'VAT', 'Ciudad del Vaticano'); 
- (51, 166, 'CC', 'CCK', 'Islas Cocos'); 
- (52, 170, 'CO', 'COL', 'Colombia'); 
- (53, 174, 'KM', 'COM', 'Comoras'); 
- (54, 180, 'CD', 'COD', 'República Democrática del Congo'); 
- (55, 178, 'CG', 'COG', 'Congo'); 
- (56, 184, 'CK', 'COK', 'Islas Cook'); 
- (57, 408, 'KP', 'PRK', 'Corea del Norte'); 
- (58, 410, 'KR', 'KOR', 'Corea del Sur'); 
- (59, 384, 'CI', 'CIV', 'Costa de Marfil'); 
- (60, 188, 'CR', 'CRI', 'Costa Rica'); 
- (61, 191, 'HR', 'HRV', 'Croacia'); 
- (62, 192, 'CU', 'CUB', 'Cuba'); 
- (63, 208, 'DK', 'DNK', 'Dinamarca'); 
- (64, 212, 'DM', 'DMA', 'Dominica'); 
- (65, 214, 'DO', 'DOM', 'República Dominicana'); 
- (66, 218, 'EC', 'ECU', 'Ecuador'); 
- (67, 818, 'EG', 'EGY', 'Egipto'); 
- (68, 222, 'SV', 'SLV', 'El Salvador'); 
- (69, 784, 'AE', 'ARE', 'Emiratos Árabes Unidos'); 
- (70, 232, 'ER', 'ERI', 'Eritrea'); 
- (71, 703, 'SK', 'SVK', 'Eslovaquia'); 
- (72, 705, 'SI', 'SVN', 'Eslovenia'); 
- (73, 724, 'ES', 'ESP', 'España'); 
- (74, 581, 'UM', 'UMI', 'Islas ultramarinas de Estados Unidos'); 
- (75, 840, 'US', 'USA', 'Estados Unidos'); 
- (76, 233, 'EE', 'EST', 'Estonia'); 
- (77, 231, 'ET', 'ETH', 'Etiopía'); 
- (78, 234, 'FO', 'FRO', 'Islas Feroe'); 
- (79, 608, 'PH', 'PHL', 'Filipinas'); 
- (80, 246, 'FI', 'FIN', 'Finlandia'); 
- (81, 242, 'FJ', 'FJI', 'Fiyi'); 
- (82, 250, 'FR', 'FRA', 'Francia'); 
- (83, 266, 'GA', 'GAB', 'Gabón'); 
- (84, 270, 'GM', 'GMB', 'Gambia'); 
- (85, 268, 'GE', 'GEO', 'Georgia'); 
- (86, 239, 'GS', 'SGS', 'Islas Georgias del Sur y Sandwich del Sur'); 
- (87, 288, 'GH', 'GHA', 'Ghana'); 
- (88, 292, 'GI', 'GIB', 'Gibraltar'); 
- (89, 308, 'GD', 'GRD', 'Granada'); 
- (90, 300, 'GR', 'GRC', 'Grecia'); 
- (91, 304, 'GL', 'GRL', 'Groenlandia'); 
- (92, 312, 'GP', 'GLP', 'Guadalupe'); 
- (93, 316, 'GU', 'GUM', 'Guam'); 
- (94, 320, 'GT', 'GTM', 'Guatemala'); 
- (95, 254, 'GF', 'GUF', 'Guayana Francesa'); 
- (96, 324, 'GN', 'GIN', 'Guinea'); 
- (97, 226, 'GQ', 'GNQ', 'Guinea Ecuatorial'); 
- (98, 624, 'GW', 'GNB', 'Guinea-Bissau'); 
- (99, 328, 'GY', 'GUY', 'Guyana'); 
- (100, 332, 'HT', 'HTI', 'Haití'); 
- (101, 334, 'HM', 'HMD', 'Islas Heard y McDonald'); 
- (102, 340, 'HN', 'HND', 'Honduras'); 
- (103, 344, 'HK', 'HKG', 'Hong Kong'); 
- (104, 348, 'HU', 'HUN', 'Hungría'); 
- (105, 356, 'IN', 'IND', 'India'); 
- (106, 360, 'ID', 'IDN', 'Indonesia'); 
- (107, 364, 'IR', 'IRN', 'Irán'); 
- (108, 368, 'IQ', 'IRQ', 'Iraq'); 
- (109, 372, 'IE', 'IRL', 'Irlanda'); 
- (110, 352, 'IS', 'ISL', 'Islandia'); 
- (111, 376, 'IL', 'ISR', 'Israel'); 
- (112, 380, 'IT', 'ITA', 'Italia'); 
- (113, 388, 'JM', 'JAM', 'Jamaica'); 
- (114, 392, 'JP', 'JPN', 'Japón'); 
- (115, 400, 'JO', 'JOR', 'Jordania'); 
- (116, 398, 'KZ', 'KAZ', 'Kazajstán'); 
- (117, 404, 'KE', 'KEN', 'Kenia'); 
- (118, 417, 'KG', 'KGZ', 'Kirguistán'); 
- (119, 296, 'KI', 'KIR', 'Kiribati'); 
- (120, 414, 'KW', 'KWT', 'Kuwait'); 
- (121, 418, 'LA', 'LAO', 'Laos'); 
- (122, 426, 'LS', 'LSO', 'Lesotho'); 
- (123, 428, 'LV', 'LVA', 'Letonia'); 
- (124, 422, 'LB', 'LBN', 'Líbano'); 
- (125, 430, 'LR', 'LBR', 'Liberia'); 
- (126, 434, 'LY', 'LBY', 'Libia'); 
- (127, 438, 'LI', 'LIE', 'Liechtenstein'); 
- (128, 440, 'LT', 'LTU', 'Lituania'); 
- (129, 442, 'LU', 'LUX', 'Luxemburgo'); 
- (130, 446, 'MO', 'MAC', 'Macao'); 
- (131, 807, 'MK', 'MKD', 'ARY Macedonia'); 
- (132, 450, 'MG', 'MDG', 'Madagascar'); 
- (133, 458, 'MY', 'MYS', 'Malasia'); 
- (134, 454, 'MW', 'MWI', 'Malawi'); 
- (135, 462, 'MV', 'MDV', 'Maldivas'); 
- (136, 466, 'ML', 'MLI', 'Malí'); 
- (137, 470, 'MT', 'MLT', 'Malta'); 
- (138, 238, 'FK', 'FLK', 'Islas Malvinas'); 
- (139, 580, 'MP', 'MNP', 'Islas Marianas del Norte'); 
- (140, 504, 'MA', 'MAR', 'Marruecos'); 
- (141, 584, 'MH', 'MHL', 'Islas Marshall'); 
- (142, 474, 'MQ', 'MTQ', 'Martinica'); 
- (143, 480, 'MU', 'MUS', 'Mauricio'); 
- (144, 478, 'MR', 'MRT', 'Mauritania'); 
- (145, 175, 'YT', 'MYT', 'Mayotte'); 
- (146, 484, 'MX', 'MEX', 'México'); 
- (147, 583, 'FM', 'FSM', 'Micronesia'); 
- (148, 498, 'MD', 'MDA', 'Moldavia'); 
- (149, 492, 'MC', 'MCO', 'Mónaco'); 
- (150, 496, 'MN', 'MNG', 'Mongolia'); 
- (151, 500, 'MS', 'MSR', 'Montserrat'); 
- (152, 508, 'MZ', 'MOZ', 'Mozambique'); 
- (153, 104, 'MM', 'MMR', 'Myanmar'); 
- (154, 516, 'NA', 'NAM', 'Namibia'); 
- (155, 520, 'NR', 'NRU', 'Nauru'); 
- (156, 524, 'NP', 'NPL', 'Nepal'); 
- (157, 558, 'NI', 'NIC', 'Nicaragua'); 
- (158, 562, 'NE', 'NER', 'Níger'); 
- (159, 566, 'NG', 'NGA', 'Nigeria'); 
- (160, 570, 'NU', 'NIU', 'Niue'); 
- (161, 574, 'NF', 'NFK', 'Isla Norfolk'); 
- (162, 578, 'NO', 'NOR', 'Noruega'); 
- (163, 540, 'NC', 'NCL', 'Nueva Caledonia'); 
- (164, 554, 'NZ', 'NZL', 'Nueva Zelanda'); 
- (165, 512, 'OM', 'OMN', 'Omán'); 
- (166, 528, 'NL', 'NLD', 'Países Bajos'); 
- (167, 586, 'PK', 'PAK', 'Pakistán'); 
- (168, 585, 'PW', 'PLW', 'Palau'); 
- (169, 275, 'PS', 'PSE', 'Palestina'); 
- (170, 591, 'PA', 'PAN', 'Panamá'); 
- (171, 598, 'PG', 'PNG', 'Papúa Nueva Guinea'); 
- (172, 600, 'PY', 'PRY', 'Paraguay'); 
- (173, 604, 'PE', 'PER', 'Perú'); 
- (174, 612, 'PN', 'PCN', 'Islas Pitcairn'); 
- (175, 258, 'PF', 'PYF', 'Polinesia Francesa'); 
- (176, 616, 'PL', 'POL', 'Polonia'); 
- (177, 620, 'PT', 'PRT', 'Portugal'); 
- (178, 630, 'PR', 'PRI', 'Puerto Rico'); 
- (179, 634, 'QA', 'QAT', 'Qatar'); 
- (180, 826, 'GB', 'GBR', 'Reino Unido'); 
- (181, 638, 'RE', 'REU', 'Reunión'); 
- (182, 646, 'RW', 'RWA', 'Ruanda'); 
- (183, 642, 'RO', 'ROU', 'Rumania'); 
- (184, 643, 'RU', 'RUS', 'Rusia'); 
- (185, 732, 'EH', 'ESH', 'Sahara Occidental'); 
- (186, 90, 'SB', 'SLB', 'Islas Salomón'); 
- (187, 882, 'WS', 'WSM', 'Samoa'); 
- (188, 16, 'AS', 'ASM', 'Samoa Americana'); 
- (189, 659, 'KN', 'KNA', 'San Cristóbal y Nevis'); 
- (190, 674, 'SM', 'SMR', 'San Marino'); 
- (191, 666, 'PM', 'SPM', 'San Pedro y Miquelón'); 
- (192, 670, 'VC', 'VCT', 'San Vicente y las Granadinas'); 
- (193, 654, 'SH', 'SHN', 'Santa Helena'); 
- (194, 662, 'LC', 'LCA', 'Santa Lucía'); 
- (195, 678, 'ST', 'STP', 'Santo Tomé y Príncipe'); 
- (196, 686, 'SN', 'SEN', 'Senegal'); 
- (197, 891, 'CS', 'SCG', 'Serbia y Montenegro'); 
- (198, 690, 'SC', 'SYC', 'Seychelles'); 
- (199, 694, 'SL', 'SLE', 'Sierra Leona'); 
- (200, 702, 'SG', 'SGP', 'Singapur'); 
- (201, 760, 'SY', 'SYR', 'Siria'); 
- (202, 706, 'SO', 'SOM', 'Somalia'); 
- (203, 144, 'LK', 'LKA', 'Sri Lanka'); 
- (204, 748, 'SZ', 'SWZ', 'Suazilandia'); 
- (205, 710, 'ZA', 'ZAF', 'Sudáfrica'); 
- (206, 736, 'SD', 'SDN', 'Sudán'); 
- (207, 752, 'SE', 'SWE', 'Suecia'); 
- (208, 756, 'CH', 'CHE', 'Suiza'); 
- (209, 740, 'SR', 'SUR', 'Surinam'); 
- (210, 744, 'SJ', 'SJM', 'Svalbard y Jan Mayen'); 
- (211, 764, 'TH', 'THA', 'Tailandia'); 
- (212, 158, 'TW', 'TWN', 'Taiwán'); 
- (213, 834, 'TZ', 'TZA', 'Tanzania'); 
- (214, 762, 'TJ', 'TJK', 'Tayikistán'); 
- (215, 86, 'IO', 'IOT', 'Territorio Británico del Océano Índico'); 
- (216, 260, 'TF', 'ATF', 'Territorios Australes Franceses'); 
- (217, 626, 'TL', 'TLS', 'Timor Oriental'); 
- (218, 768, 'TG', 'TGO', 'Togo'); 
- (219, 772, 'TK', 'TKL', 'Tokelau'); 
- (220, 776, 'TO', 'TON', 'Tonga'); 
- (221, 780, 'TT', 'TTO', 'Trinidad y Tobago'); 
- (222, 788, 'TN', 'TUN', 'Túnez'); 
- (223, 796, 'TC', 'TCA', 'Islas Turcas y Caicos'); 
- (224, 795, 'TM', 'TKM', 'Turkmenistán'); 
- (225, 792, 'TR', 'TUR', 'Turquía'); 
- (226, 798, 'TV', 'TUV', 'Tuvalu'); 
- (227, 804, 'UA', 'UKR', 'Ucrania'); 
- (228, 800, 'UG', 'UGA', 'Uganda'); 
- (229, 858, 'UY', 'URY', 'Uruguay'); 
- (230, 860, 'UZ', 'UZB', 'Uzbekistán'); 
- (231, 548, 'VU', 'VUT', 'Vanuatu'); 
- (232, 862, 'VE', 'VEN', 'Venezuela'); 
- (233, 704, 'VN', 'VNM', 'Vietnam'); 
- (234, 92, 'VG', 'VGB', 'Islas Vírgenes Británicas'); 
- (235, 850, 'VI', 'VIR', 'Islas Vírgenes de los Estados Unidos'); 
- (236, 876, 'WF', 'WLF', 'Wallis y Futuna'); 
- (237, 887, 'YE', 'YEM', 'Yemen'); 
- (238, 262, 'DJ', 'DJI', 'Yibuti'); 
- (239, 894, 'ZM', 'ZMB', 'Zambia'); 
- (240, 716, 'ZW', 'ZWE', 'Zimbabue'); 
+	('AND', 'Andorra'), 
+	('AGO', 'Angola'), 
+	('AIA', 'Anguilla'), 
+	('ATA', 'Antártida'), 
+	('ATG', 'Antigua y Barbuda'), 
+	('ANT', 'Antillas Holandesas'), 
+	('SAU', 'Arabia Saudí'), 
+	('DZA', 'Argelia'), 
+	('ARG', 'Argentina'), 
+	('ARM', 'Armenia'), 
+	('ABW', 'Aruba'), 
+	('AUS', 'Australia'), 
+	('AUT', 'Austria'), 
+	('AZE', 'Azerbaiyán'), 
+	('BHS', 'Bahamas'), 
+	('BHR', 'Bahréin'), 
+	('BGD', 'Bangladesh'), 
+	('BRB', 'Barbados'), 
+	('BLR', 'Bielorrusia'), 
+	('BEL', 'Bélgica'), 
+	('BLZ', 'Belice'),
+	('BEN', 'Benin'), 
+	('BMU', 'Bermudas'), 
+	('BTN', 'Bhután'), 
+	('BOL', 'Bolivia'), 
+	('BIH', 'Bosnia y Herzegovina'), 
+	('BWA', 'Botsuana'), 
+	('BVT', 'Isla Bouvet'), 
+	('BRA', 'Brasil'), 
+	('BRN', 'Brunéi'), 
+	('BGR', 'Bulgaria'), 
+	('BFA', 'Burkina Faso'), 
+	('BDI', 'Burundi'), 
+	('CPV', 'Cabo Verde'), 
+	('CYM', 'Islas Caimán'), 
+	('KHM', 'Camboya'), 
+	('CMR', 'Camerún'), 
+	('CAN', 'Canadá'), 
+	('CAF', 'República Centroafricana'), 
+	('TCD', 'Chad'), 
+	('CZE', 'República Checa'), 
+	('CHL', 'Chile'), 
+	('CHN', 'China'), 
+	('CYP', 'Chipre'), 
+	('CXR', 'Isla de Navidad'), 
+	('VAT', 'Ciudad del Vaticano'), 
+	('CCK', 'Islas Cocos'), 
+	('COL', 'Colombia'), 
+	('COM', 'Comoras'), 
+	('COD', 'República Democrática del Congo'), 
+	('COG', 'Congo'), 
+	('COK', 'Islas Cook'), 
+	('PRK', 'Corea del Norte'), 
+	('KOR', 'Corea del Sur'), 
+	('CIV', 'Costa de Marfil'), 
+	('CRI', 'Costa Rica'), 
+	('HRV', 'Croacia'), 
+	('CUB', 'Cuba'), 
+	('DNK', 'Dinamarca'), 
+	('DMA', 'Dominica'), 
+	('DOM', 'República Dominicana'), 
+	('ECU', 'Ecuador'), 
+	('EGY', 'Egipto'), 
+	('SLV', 'El Salvador'), 
+	('ARE', 'Emiratos Árabes Unidos'), 
+	('ERI', 'Eritrea'), 
+	('SVK', 'Eslovaquia'), 
+	('SVN', 'Eslovenia'), 
+	('ESP', 'España'), 
+	('UMI', 'Islas ultramarinas de Estados Unidos'), 
+	('USA', 'Estados Unidos'), 
+	('EST', 'Estonia'), 
+	('ETH', 'Etiopía'), 
+	('FRO', 'Islas Feroe'), 
+	('PHL', 'Filipinas'), 
+	('FIN', 'Finlandia'), 
+	('FJI', 'Fiyi'), 
+	('FRA', 'Francia'), 
+	('GAB', 'Gabón'), 
+	('GMB', 'Gambia'), 
+	('GEO', 'Georgia'), 
+	('SGS', 'Islas Georgias del Sur y Sandwich del Sur'), 
+	('GHA', 'Ghana'), 
+	('GIB', 'Gibraltar'), 
+	('GRD', 'Granada'), 
+	('GRC', 'Grecia'), 
+	('GRL', 'Groenlandia'), 
+	('GLP', 'Guadalupe'), 
+	('GUM', 'Guam'), 
+	('GTM', 'Guatemala'), 
+	('GUF', 'Guayana Francesa'), 
+	('GIN', 'Guinea'), 
+	('GNQ', 'Guinea Ecuatorial'), 
+	('GNB', 'Guinea-Bissau'), 
+	('GUY', 'Guyana'), 
+	('HTI', 'Haití'), 
+	('HMD', 'Islas Heard y McDonald'), 
+	('HND', 'Honduras'), 
+	('HKG', 'Hong Kong'), 
+	('HUN', 'Hungría'), 
+	('IND', 'India'), 
+	('IDN', 'Indonesia'), 
+	('IRN', 'Irán'), 
+	('IRQ', 'Iraq'), 
+	('IRL', 'Irlanda'), 
+	('ISL', 'Islandia'), 
+	('ISR', 'Israel'), 
+	('ITA', 'Italia'), 
+	('JAM', 'Jamaica'), 
+	('JPN', 'Japón'), 
+	('JOR', 'Jordania'), 
+	('KAZ', 'Kazajstán'), 
+	('KEN', 'Kenia'), 
+	('KGZ', 'Kirguistán'), 
+	('KIR', 'Kiribati'), 
+	('KWT', 'Kuwait'), 
+	('LAO', 'Laos'), 
+	('LSO', 'Lesotho'), 
+	('LVA', 'Letonia'), 
+	('LBN', 'Líbano'), 
+	('LBR', 'Liberia'), 
+	('LBY', 'Libia'), 
+	('LIE', 'Liechtenstein'), 
+	('LTU', 'Lituania'), 
+	('LUX', 'Luxemburgo'), 
+	('MAC', 'Macao'), 
+	('MKD', 'ARY Macedonia'), 
+	('MDG', 'Madagascar'), 
+	('MYS', 'Malasia'), 
+	('MWI', 'Malawi'), 
+	('MDV', 'Maldivas'), 
+	('MLI', 'Malí'), 
+	('MLT', 'Malta'), 
+	('FLK', 'Islas Malvinas'), 
+	('MNP', 'Islas Marianas del Norte'), 
+	('MAR', 'Marruecos'), 
+	('MHL', 'Islas Marshall'), 
+	('MTQ', 'Martinica'), 
+	('MUS', 'Mauricio'), 
+	('MRT', 'Mauritania'), 
+	('MYT', 'Mayotte'), 
+	('MEX', 'México'), 
+	('FSM', 'Micronesia'), 
+	('MDA', 'Moldavia'), 
+	('MCO', 'Mónaco'), 
+	('MNG', 'Mongolia'), 
+	('MSR', 'Montserrat'), 
+	('MOZ', 'Mozambique'), 
+	('MMR', 'Myanmar'), 
+	('NAM', 'Namibia'), 
+	('NRU', 'Nauru'), 
+	('NPL', 'Nepal'), 
+	('NIC', 'Nicaragua'), 
+	('NER', 'Níger'), 
+	('NGA', 'Nigeria'), 
+	('NIU', 'Niue'), 
+	('NFK', 'Isla Norfolk'), 
+	('NOR', 'Noruega'), 
+	('NCL', 'Nueva Caledonia'), 
+	('NZL', 'Nueva Zelanda'), 
+	('OMN', 'Omán'), 
+	('NLD', 'Países Bajos'), 
+	('PAK', 'Pakistán'), 
+	('PLW', 'Palau'), 
+	('PSE', 'Palestina'), 
+	('PAN', 'Panamá'), 
+	('PNG', 'Papúa Nueva Guinea'), 
+	('PRY', 'Paraguay'), 
+	('PER', 'Perú'), 
+	('PCN', 'Islas Pitcairn'), 
+	('PYF', 'Polinesia Francesa'), 
+	('POL', 'Polonia'), 
+	('PRT', 'Portugal'), 
+	('PRI', 'Puerto Rico'), 
+	('QAT', 'Qatar'), 
+	('GBR', 'Reino Unido'), 
+	('REU', 'Reunión'), 
+	('RWA', 'Ruanda'), 
+	('ROU', 'Rumania'), 
+	('RUS', 'Rusia'), 
+	('ESH', 'Sahara Occidental'), 
+	('SLB', 'Islas Salomón'), 
+	('WSM', 'Samoa'), 
+	('ASM', 'Samoa Americana'), 
+	('KNA', 'San Cristóbal y Nevis'), 
+	('SMR', 'San Marino'), 
+	('SPM', 'San Pedro y Miquelón'), 
+	('VCT', 'San Vicente y las Granadinas'), 
+	('SHN', 'Santa Helena'), 
+	('LCA', 'Santa Lucía'), 
+	('STP', 'Santo Tomé y Príncipe'), 
+	('SEN', 'Senegal'), 
+	('SCG', 'Serbia y Montenegro'), 
+	('SYC', 'Seychelles'), 
+	('SLE', 'Sierra Leona'), 
+	('SGP', 'Singapur'), 
+	('SYR', 'Siria'), 
+	('SOM', 'Somalia'), 
+	('LKA', 'Sri Lanka'), 
+	('SWZ', 'Suazilandia'), 
+	('ZAF', 'Sudáfrica'), 
+	('SDN', 'Sudán'), 
+	('SWE', 'Suecia'), 
+	('CHE', 'Suiza'), 
+	('SUR', 'Surinam'), 
+	('SJM', 'Svalbard y Jan Mayen'), 
+	('THA', 'Tailandia'), 
+	('TWN', 'Taiwán'), 
+	('TZA', 'Tanzania'), 
+	('TJK', 'Tayikistán'), 
+	('IOT', 'Territorio Británico del Océano Índico'), 
+	('ATF', 'Territorios Australes Franceses'), 
+	('TLS', 'Timor Oriental'), 
+	('TGO', 'Togo'), 
+	('TKL', 'Tokelau'), 
+	('TON', 'Tonga'), 
+	('TTO', 'Trinidad y Tobago'), 
+	('TUN', 'Túnez'), 
+	('TCA', 'Islas Turcas y Caicos'), 
+	('TKM', 'Turkmenistán'), 
+	('TUR', 'Turquía'), 
+	('TUV', 'Tuvalu'), 
+	('UKR', 'Ucrania'), 
+	('UGA', 'Uganda'), 
+	('URY', 'Uruguay'), 
+	('UZB', 'Uzbekistán'), 
+	('VUT', 'Vanuatu'), 
+	('VEN', 'Venezuela'), 
+	('VNM', 'Vietnam'), 
+	('VGB', 'Islas Vírgenes Británicas'), 
+	('VIR', 'Islas Vírgenes de los Estados Unidos'), 
+	('WLF', 'Wallis y Futuna'), 
+	('YEM', 'Yemen'), 
+	('DJI', 'Yibuti'), 
+	('ZMB', 'Zambia'), 
+	('ZWE', 'Zimbabue')
+ 
 
-
-
-
-
-
-
+ 
 insert into usuario values
 	()
 
--- Script-restricciones.sql
+++ Script-restricciones.sql
@@ -83,21 +83,37 @@ alter column chatId int not Null
 alter table chatParticipante 
 alter column usuarioParticipante int not Null
 
-alter table chat 
-add chatId int primary key
+Alter table chatParticipante
+add Constraint Pk_chatId_usuarioParticipante Primary Key (chatId, usuarioParticipante)
 
+alter table chatParticipante
+Add Constraint fk_chatId foreign Key (chatId) references chat(chatId)
 
-
--Las 2 son foreignkey
--Las 2 son PrimaryKey
+alter table chatParticipante
+Add Constraint fk_usuarioParticipante foreign Key (usuarioParticipante) references usuario(usuarioId)
 
 --CREATE TABLE grupoAdmin(
 --chatId int, 
 --usuarioId int
 --)
 
--Las 2 son foreignkey
--Las 2 son PrimaryKey
+Alter table grupoAdmin
+alter column chatId int not Null
+
+Alter table grupoAdmin
+alter column usuarioId int not Null
+
+Alter table grupoAdmin
+add Constraint Pk_chatId_usuarioId Primary Key (chatId, usuarioId)
+
+alter table grupoAdmin
+Add Constraint fk_chatId_grupoAdmin foreign Key (chatId) references chat(chatId)
+
+alter table grupoAdmin
+Add Constraint fk_usuarioId_grupoAdmin foreign Key (usuarioId) references usuario(usuarioId)
+
+
+
 
 --CREATE TABLE mensaje(
 --mensajeId int, 
@@ -110,20 +126,48 @@ add chatId int primary key
 --archivoId int
 --)
 
-mensajeId - Primary
-chatId - foreign de chat participante.
-usuarioId - foreign
-mensajeTipo - Chek - Texto, Audio, Video, Imagen
-mensajeEstado - Chek - Enviado, Pendiente
-archivoId - Foreign de Archivo
+Alter table mensaje
+alter column mensajeId int not Null
+
+Alter table mensaje
+add Constraint Pk_mensajeId Primary Key (mensajeId)
+
+
+alter table mensaje
+Add Constraint fk_chatId_mensaje foreign Key (chatId) references chat(chatId)
+
+alter table mensaje
+Add Constraint fk_usuarioId_mensaje foreign Key (usuarioId) references usuario(usuarioId)
+
+alter table mensaje
+Add Constraint fk_archivoId_mensaje foreign Key (archivoId) references archivo(archivoId)
+
+
+alter table mensaje
+add constraint ck_mensajeTipo check (mensajeTipo IN ('Texto', 'Audio', 'Video', 'Imagen'))
+
+alter table mensaje
+add constraint ck_mensajeEstado check (mensajeEstado IN ('Enviado', 'Pendiente'))
+
+
+
 
 --CREATE TABLE archivo(
 --archivoId int, 
 --archivoContenido VARBINARY(MAX)
 --)
 
-archivoId - Primary Identity
-Todos requeridos
+alter table archivo drop column archivoId
+
+alter table archivo 
+add archivoId int primary key identity
+
+
+Alter table archivo
+alter column archivoContenido int not Null
+
+
+
 
 --CREATE TABLE llamada(
 --llamador int, 
@@ -134,8 +178,28 @@ Todos requeridos
 --respondida bit
 --)
 
-llamador y receptor - Foreign de usuario
-respondida Not Null
+Alter table llamada
+alter column llamador int not Null
+
+Alter table llamada
+alter column receptor int not Null
+
+Alter table llamada
+alter column fechaComienzo Datetime not Null
+
+Alter table llamada
+alter column respondida bit not Null
+
+Alter table llamada
+add Constraint Pk_llamador_receptor_fchaComienzo Primary Key (llamador, receptor, fechaComienzo)
+
+
+alter table llamada
+Add Constraint fk_llamador foreign Key (llamador) references usuario(usuarioId)
+
+alter table llamada
+Add Constraint fk_receptor foreign Key (receptor) references usuario(usuarioId)
+
 
 
 --CREATE TABLE contacto(
@@ -143,7 +207,23 @@ respondida Not Null
 --contactoId int
 --)
 
-usuarioId y contactoId son foreingkey y primary
+
+Alter table contacto
+alter column usuarioId int not Null
+
+Alter table contacto
+alter column contactoId int not Null
+
+Alter table contacto
+add Constraint Pk_usuarioId_contactoId Primary Key (usuarioId, contactoId)
+
+alter table contacto
+Add Constraint fk_usuarioId_contacto foreign Key (usuarioId) references usuario(usuarioId)
+
+alter table contacto
+Add Constraint fk_contactoId_contacto foreign Key (contactoId) references usuario(usuarioId)
+
+
 
 --CREATE TABLE bloqueado(
 --usuarioId int, 
@@ -151,8 +231,22 @@ usuarioId y contactoId son foreingkey y primary
 --fechaBloqueado datetime
 --)
 
-usuarioId y contactoBloqueadoId primary
-usuarioId y contactoBloqueadoId son foreign
+Alter table bloqueado
+alter column usuarioId int not Null
+
+Alter table bloqueado
+alter column contactoBloqueadoId int not Null
+
+Alter table bloqueado
+add Constraint Pk_usuarioId_contactoBloqueadoId Primary Key (usuarioId, contactoBloqueadoId)
+
+alter table bloqueado
+Add Constraint fk_usuarioId_bloqueado foreign Key (usuarioId) references usuario(usuarioId)
+
+alter table bloqueado
+Add Constraint fk_contactoBloqueadoId foreign Key (contactoBloqueadoId) references usuario(usuarioId)
+
+
 
 
 
